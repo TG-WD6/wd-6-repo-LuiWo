d5e2c6119477b342db18101f89e8f29b40801886
Luisa Wolter <93677938+LuiWo@users.noreply.github.com>
2022-06-14 16:34:57 +0200

    JS Functions opdr 1 t/m 3

---
+++ JavaScript/Functions/Opdr4.1/script.js
@@ -0,0 +1,29 @@
+console.log("Hello world");
+
+// Opdr 1)
+
+function pressButtone() {
+    console.log("Hello World");
+}
+
+
+// Opdr 2) 
+var input = document.getElementById(inputField);
+
+
+// Opdr 3)
+function change(input) {
+    console.log(input);
+    return console.log(input * 2); ; 
+}
+
+ 
+// Opr 4)
+var input = document.getElementById(inputField);
+
+
+function change(input) {
+    console.log(input);
+    var result = console.log(input * 2);
+    return result ; 
+}
---
+++ JavaScript/Functions/Opdr4.4/script.js
@@ -0,0 +1,18 @@
+
+var names = (Esther, Madhwi, Nathan);
+var names = document.get
+
+function nameChecker(names) {
+    // Use DOM to get the input value and save it in the above var 
+  if (names == Esther || names == Madhwi || names == Nathan) {
+    console.log("Welcome " + names) 
+  } else { console.log("Names no recognized") }
+
+};
+
+
+
+
+// function placeResults(); {
+
+// }
